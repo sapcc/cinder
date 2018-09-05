d74a88d9ebd79ac00e7d7d50101b2e25611d1b3a
Fabian Wiesel <fabian.wiesel@sap.com>
2018-09-05 13:34:02 +0200

    Fixup to double spending migration
    Most important change: Do not use _and_ statement, as it will replace it with a expression-object
    Also instead of using rank, do a join, which might or might not be better

--- cinder/db/sqlalchemy/migrate_repo/versions/119_fix_quota_double_spending.py
+++ cinder/db/sqlalchemy/migrate_repo/versions/119_fix_quota_double_spending.py
@@ -14,7 +14,7 @@
 
 from migrate.changeset.constraint import UniqueConstraint
 from sqlalchemy import MetaData, Table
-from sqlalchemy import select, func
+from sqlalchemy import select, join, and_
 from oslo_log import log as logging
 
 LOG = logging.getLogger(__name__)
@@ -27,7 +27,8 @@ def quota_usages_table(migrate_engine):
 
 
 def _build_constraint(migrate_engine, quota_usages=None):
-    quota_usages = quota_usages or quota_usages_table(migrate_engine)
+    if quota_usages is None:
+        quota_usages = quota_usages_table(migrate_engine)
     return UniqueConstraint(
         'project_id', 'resource', 'deleted',
         table=quota_usages,
@@ -36,22 +37,26 @@ def _build_constraint(migrate_engine, quota_usages=None):
 
 def upgrade(migrate_engine):
     quota_usages = quota_usages_table(migrate_engine)
-    uniqueness = [quota_usages.c.project_id,
-         quota_usages.c.resource,
-         quota_usages.c.deleted
-         ]
 
-    # Get all quota_usages ranked by their id
-    rankquery = select([quota_usages.c.id, func.dense_rank().over(
-        partition_by=uniqueness, order_by=quota_usages.c.id
-    ).label('rank')]).alias('qu')
+    qu1 = quota_usages.alias('qu1')
+    qu2 = quota_usages.alias('qu2')
+    duplicates = select([qu1.c.id]).select_from(join(
+        qu1, qu2,
+        onclause=and_(
+            qu1.c.project_id == qu2.c.project_id,
+            qu1.c.resource == qu2.c.resource,
+            qu1.c.deleted == qu2.c.deleted,
+        )
+    )).where(
+        qu1.c.id>qu2.c.id
+    )
 
-    # Filter out the first ones (leaving the duplicates)
-    filterquery = select([rankquery.c.id]).where(rankquery.c.rank > 1)
 
-    # Delete all later duplicates
-    query = quota_usages.delete().where(quota_usages.c.id.in_(filterquery))
-    migrate_engine.execute(query)
+    query = quota_usages.delete(
+        whereclause=quota_usages.c.id.in_(duplicates)
+    )
+
+    result = migrate_engine.execute(query)
     cons = _build_constraint(migrate_engine, quota_usages=quota_usages)
     cons.create()
 
