f8381370a597bdba783d35f8f50896ab9d361184
Fabian Wiesel <fabian.wiesel@sap.com>
2018-09-05 11:57:02 +0200

    Delete duplicate quota usage tables

--- cinder/db/sqlalchemy/migrate_repo/versions/119_fix_quota_double_spending.py
+++ cinder/db/sqlalchemy/migrate_repo/versions/119_fix_quota_double_spending.py
@@ -14,20 +14,45 @@
 
 from migrate.changeset.constraint import UniqueConstraint
 from sqlalchemy import MetaData, Table
+from sqlalchemy import select, func
+from oslo_log import log as logging
 
+LOG = logging.getLogger(__name__)
 
-def _build_constraint(migrate_engine):
+def quota_usages_table(migrate_engine):
     meta = MetaData()
     meta.bind = migrate_engine
-    table = Table('quota_usages', meta, autoload=True)
+
+    return Table('quota_usages', meta, autoload=True)
+
+
+def _build_constraint(migrate_engine, quota_usages=None):
+    quota_usages = quota_usages or quota_usages_table(migrate_engine)
     return UniqueConstraint(
         'project_id', 'resource', 'deleted',
-        table=table,
+        table=quota_usages,
     )
 
 
 def upgrade(migrate_engine):
-    cons = _build_constraint(migrate_engine)
+    quota_usages = quota_usages_table(migrate_engine)
+    uniqueness = [quota_usages.c.project_id,
+         quota_usages.c.resource,
+         quota_usages.c.deleted
+         ]
+
+    # Get all quota_usages ranked by their id
+    rankquery = select([quota_usages.c.id, func.dense_rank().over(
+        partition_by=uniqueness, order_by=quota_usages.c.id
+    ).label('rank')]).alias('qu')
+
+    # Filter out the first ones (leaving the duplicates)
+    filterquery = select([rankquery.c.id]).where(rankquery.c.rank > 1)
+
+    # Delete all later duplicates
+    query = quota_usages.delete().where(quota_usages.c.id.in_(filterquery))
+    migrate_engine.execute(query)
+    cons = _build_constraint(migrate_engine, quota_usages=quota_usages)
     cons.create()
 
 
