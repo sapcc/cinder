43a673aa520857b8ac0579c91ee54f21d22b05f5
Marius Leustean <marius.leustean@sap.com>
2022-07-17 15:19:20 +0300

    [SAP] Migrate the volume if resize can't happen on current host
    If the new size of a volume isn't accepted by the host
    it will be rescheduled and migrated to another one.
    Volumes that are in a group cannot be rescheduled because
    migration to another host is not possible in that case.
    
    Change-Id: Ibf063e9a59b1fbd9fe97389ef4621c7d579a149a

--- cinder/volume/api.py
+++ cinder/volume/api.py
@@ -356,7 +356,7 @@ class API(base.Base):
         if not metadata:
             metadata = {}
 
-        metadata = self._set_scheduler_hints_to_volume_metadata(
+        metadata = volume_utils.set_scheduler_hints_to_volume_metadata(
             scheduler_hints,
             metadata
         )
@@ -932,7 +932,8 @@ class API(base.Base):
             'volume_id': volume.id}
 
         # Check if there is an affinity/antiaffinity against the volume
-        filter_properties = self._get_scheduler_hints_from_volume(volume)
+        filter_properties = \
+            volume_utils.get_scheduler_hints_from_volume(volume)
 
         try:
             dest = self.scheduler_rpcapi.find_backend_for_connector(
@@ -1938,7 +1939,7 @@ class API(base.Base):
             raise exception.InvalidVolume(reason=msg)
 
         # Check if there is an affinity/antiaffinity against the volume
-        filter_props = self._get_scheduler_hints_from_volume(volume)
+        filter_props = volume_utils.get_scheduler_hints_from_volume(volume)
 
         # Call the scheduler to ensure that the host exists and that it can
         # accept the volume
