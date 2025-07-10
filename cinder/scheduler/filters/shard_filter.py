ce0a5a3dd0c093c512130833240abecd46843562
Walter Boring <waboring@hemna.com>
2025-07-10 11:49:10 -0400

    [SAP] Added new SAPShardRetypeMigrationFilter
    The purpose of this filter is to only allow retypes
    and migrations of vmdk -> fcd volumes to land on the same
    shard.  We shouldn't allow vmdk -> fcd to happen accross vcenters.
    
    Change-Id: Ic7b5df9367c0eaf17d13f6953f1556167768937a
    (cherry picked from commit d293ae3894ffcd2d4c5e4d54df0a46b67c726612)

--- cinder/scheduler/filters/shard_filter.py
+++ cinder/scheduler/filters/shard_filter.py
@@ -246,3 +246,55 @@ class ShardFilter(filters.BaseBackendFilter):
                        'backend_shards': configured_shards_set,
                        'project_shards': shards})
             return False
+
+
+class SAPShardRetypeMigrationFilter(ShardFilter):
+    """Filters backends by shard of the project
+
+    When a volume is retyping or migrating from vmdk -> vstorageobject, we want
+    to ensure that the volume is migrated to the same shard as the original
+    volume.
+
+    """
+
+    def backend_passes(self, backend_state, filter_properties):
+        # We only need the shard filter for vmware based pools
+        if backend_state.vendor_name != 'VMware':
+            LOG.info(
+                "Shard Filter ignoring backend %s as it's not vmware based"
+                " driver", backend_state.backend_id)
+            return True
+
+        spec = filter_properties.get('request_spec', {})
+        vol = spec.get('volume_properties', {})
+
+        # Any retyping of a vmware based volume should land on the same shard.
+        if spec.get('operation') == 'retype_volume':
+            # The backend can only be on the same shard as
+            # the volume is currently on.
+            vol_shard = self._extract_shard_from_host(vol['host'])
+            backend_shard = self._extract_shard_from_host(backend_state.host)
+            if vol_shard == backend_shard:
+                return True
+            else:
+                return False
+
+        # A migration for vmdk -> fcd(vstorageobject) should land
+        # on the same shard.
+        if spec.get('operation') == 'migrate_volume':
+            # The backend can only be on the same shard as
+            # the volume is currently on.
+            # host entries for vmdk base volumes are
+            #    '<system_name>@vmware#pool'
+            # host entries for fcd base volumes are
+            #    '<system_name>@vmware_fcd#pool'
+            vol_backend = cinder_utils.extract_host(
+                vol['host'], 'backend').split('@')[1]
+            backend_backend = cinder_utils.extract_host(
+                backend_state.host, 'backend').split('@')[1]
+            vol_shard = self._extract_shard_from_host(vol['host'])
+            backend_shard = self._extract_shard_from_host(backend_state.host)
+            if (vol_backend == 'vmware' and backend_backend == 'vmware_fcd' and
+                    vol_shard == backend_shard):
+                return True
+            return False
