b0cacade375493e795c6412af25dee81d820e822
Csaba Seres <csaba.seres@gmail.com>
2025-12-09 12:45:53 +0100

    use zapi in _get_ssc_flexvol_info Looks like REST api is not thread safe, failing from loopingcall
    Change-Id: Iccd1edcfaca2827f4eaa962b7e0d136962ba668c

--- cinder/tests/unit/volume/drivers/netapp/dataontap/utils/test_capabilities.py
+++ cinder/tests/unit/volume/drivers/netapp/dataontap/utils/test_capabilities.py
@@ -158,6 +158,9 @@ class CapabilitiesLibraryTestCase(test.TestCase):
         self.mock_object(self.ssc_library.zapi_client,
                          'get_flexvol',
                          return_value=fake_client.VOLUME_INFO_SSC)
+        self.mock_object(self.ssc_library.zapi_client,
+                         'get_flexvol_zapi',
+                         return_value=fake_client.VOLUME_INFO_SSC)
 
         result = self.ssc_library._get_ssc_flexvol_info(
             fake_client.VOLUME_NAMES[0])
@@ -170,7 +173,7 @@ class CapabilitiesLibraryTestCase(test.TestCase):
             'netapp_is_flexgroup': 'false',
         }
         self.assertEqual(expected, result)
-        self.zapi_client.get_flexvol.assert_called_once_with(
+        self.zapi_client.get_flexvol_zapi.assert_called_once_with(
             flexvol_name=fake_client.VOLUME_NAMES[0])
 
     @ddt.data({'vol_space_guarantee': 'file', 'lun_space_guarantee': True},
@@ -186,6 +189,9 @@ class CapabilitiesLibraryTestCase(test.TestCase):
         self.mock_object(self.ssc_library.zapi_client,
                          'get_flexvol',
                          return_value=fake_volume_info_ssc)
+        self.mock_object(self.ssc_library.zapi_client,
+                         'get_flexvol_zapi',
+                         return_value=fake_volume_info_ssc)
 
         result = self.ssc_library._get_ssc_flexvol_info(
             fake_client.VOLUME_NAMES[0])
@@ -198,7 +204,7 @@ class CapabilitiesLibraryTestCase(test.TestCase):
             'netapp_is_flexgroup': 'false',
         }
         self.assertEqual(expected, result)
-        self.zapi_client.get_flexvol.assert_called_once_with(
+        self.zapi_client.get_flexvol_zapi.assert_called_once_with(
             flexvol_name=fake_client.VOLUME_NAMES[0])
 
     @ddt.data({'nfs_sparsed_volumes': True},
@@ -212,6 +218,9 @@ class CapabilitiesLibraryTestCase(test.TestCase):
         self.mock_object(self.ssc_library.zapi_client,
                          'get_flexvol',
                          return_value=fake_client.VOLUME_INFO_SSC)
+        self.mock_object(self.ssc_library.zapi_client,
+                         'get_flexvol_zapi',
+                         return_value=fake_client.VOLUME_INFO_SSC)
 
         result = self.ssc_library._get_ssc_flexvol_info(
             fake_client.VOLUME_NAMES[0])
@@ -224,7 +233,7 @@ class CapabilitiesLibraryTestCase(test.TestCase):
             'netapp_is_flexgroup': 'false',
         }
         self.assertEqual(expected, result)
-        self.zapi_client.get_flexvol.assert_called_once_with(
+        self.zapi_client.get_flexvol_zapi.assert_called_once_with(
             flexvol_name=fake_client.VOLUME_NAMES[0])
 
     @ddt.data({'vol_space_guarantee': 'file', 'nfs_sparsed_volumes': True},
@@ -241,6 +250,9 @@ class CapabilitiesLibraryTestCase(test.TestCase):
         self.mock_object(self.ssc_library.zapi_client,
                          'get_flexvol',
                          return_value=fake_volume_info_ssc)
+        self.mock_object(self.ssc_library.zapi_client,
+                         'get_flexvol_zapi',
+                         return_value=fake_volume_info_ssc)
 
         result = self.ssc_library._get_ssc_flexvol_info(
             fake_client.VOLUME_NAMES[0])
@@ -253,7 +265,7 @@ class CapabilitiesLibraryTestCase(test.TestCase):
             'netapp_is_flexgroup': 'false',
         }
         self.assertEqual(expected, result)
-        self.zapi_client.get_flexvol.assert_called_once_with(
+        self.zapi_client.get_flexvol_zapi.assert_called_once_with(
             flexvol_name=fake_client.VOLUME_NAMES[0])
 
     @ddt.data([], ['netapp_dedup'], ['netapp_compression'])
