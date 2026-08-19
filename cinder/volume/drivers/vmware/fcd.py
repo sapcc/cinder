24b4ee9469e39b0c87eeb71d2cc44662cf32bf7d
Csaba Seres <45421502+Scsabiii@users.noreply.github.com>
2026-08-19 14:06:13 +0200

    Add call to extend_volume from terminate_connection in case KVM host (#359)
    * Add call to extend_volume from terminate_connection in case KVM host
    Change-Id: Ia6616797d207bb5dbd912c8d81164376adb0eb27

--- cinder/volume/drivers/vmware/fcd.py
+++ cinder/volume/drivers/vmware/fcd.py
@@ -628,6 +628,19 @@ class VMwareVStorageObjectDriver(vmdk.VMwareVcVmdkDriver):
 
     @volume_utils.trace
     def terminate_connection(self, volume, connector, force=False, **kwargs):
+        if (connector and ('connection_capabilities' not in connector)):
+            try:
+                # If the volume was extended on KVM host, the VMware VMDK file
+                # still contains the old size, so to refresh geometry we call
+                # VslmExtendDisk_Task to fix it
+                fcd_loc = vops.FcdLocation.from_provider_location(
+                    self._provider_location_to_moref_location(
+                        volume.provider_location
+                    )
+                )
+                self.volumeops.extend_fcd(fcd_loc, volume.size * units.Ki)
+            except Exception:
+                pass
         # Checking if the connection was used to restore from a backup. In
         # that case, the VMDK connector in os-brick created a new backing
         # which will replace the initial one. Here we set the proper name
